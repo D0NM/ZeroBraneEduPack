bc571bcbdaf647f1c091239730af53f15d55d1ec
Don Miguel <D0NM@users.noreply.github.com>
2018-10-04 16:54:21 +0500

    Update pixels.lua
    Hide turtle to speed up the process of drawing.

--- turtle-samples/pixels.lua
+++ turtle-samples/pixels.lua
@@ -4,6 +4,7 @@ local x,y = size() -- get the current screen size
 local maxc = 256
 updt(false) -- disable auto updates
 zero(0, 0) -- change origin such that (0,0) is now in the left top corner
+hide()
 for row = 0, x do
   for col = 0, y do
     local res = row^2 + col^2 -- also try using "row * col"
